5cc08bd88a6f8bc29fe45c2d911c8c8f77cac9d9
Claudia Hernandez <hernandez.clauds@gmail.com>
2018-06-09 21:09:28 -0400

    moved JS back into script LOL

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-window.addEventListener('keydown', function(e) {
-  const audio = document.querySelector(`audio[data-key= "${e.keyCode}"]`);
-  if(!audio) return; //stops the function from running all together
-  audio.currentTime = 0; //rewinds sound to beggining
-  audio.play();
-  
-  })
+
+
